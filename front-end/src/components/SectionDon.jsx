7c01f2b03fe61be281f2c116502b652bd1f9b91d
siapri21 <siapriouattara21@gmail.com>
2024-10-22 17:02:27 +0200

    mise en place du back-end

--- front-end/src/components/SectionDon.jsx
+++ front-end/src/components/SectionDon.jsx
@@ -4,7 +4,7 @@ const SectionDon = () => {
     return (
         <div className="don-section flex justify-center h-full w-full bg-slate-700">
             <div className="container mx-auto p-4 pt-6 md:p-6 lg:p">
-                <h1 className='text-center bg-red-300'>Lien tire d'une autre site </h1>
+                <h1 className='text-center bg-red-300'>section donation </h1>
             </div>
 
         </div>
