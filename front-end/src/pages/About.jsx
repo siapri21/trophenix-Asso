70471ef908fc28678b3b52a2afc20e65b6a419aa
siapri21 <siapriouattara21@gmail.com>
2024-11-07 01:19:14 +0100

    animation et reaction des contenue page about et events

--- front-end/src/pages/About.jsx
+++ front-end/src/pages/About.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Slide, Zoom } from '@mui/material';
 import { ReactTyped } from 'react-typed';
+import { Fade } from '@mui/material';
+import { useInView } from 'react-intersection-observer';
+import { Link } from 'react-router-dom';
+
+
 
 const About = () => {
   const [checked, setChecked] = useState(true);
@@ -9,6 +14,10 @@ const About = () => {
     setChecked(true); // Active l'animation au chargement
   }, []);
 
+    const { ref, inView } = useInView({
+      triggerOnce: true, // l'animation ne s'active qu'une seule fois
+      threshold: 0.3,    // déclenche l'animation lorsque 30% de la section est visible
+    });
  
 
   return (
@@ -32,7 +41,7 @@ const About = () => {
         </div>
         <div className="relative w-full md:w-1/2 mb-6 md:mb-0">
           <img
-            src="/asserts/img/131.png"
+            src="/asserts/img/2.png"
             alt="Group of people"
             className="h-60 transform skew-x-12 rounded-lg"
           />
@@ -56,7 +65,8 @@ const About = () => {
           </p>
           <h2 className="text-3xl md:text-4xl font-bold text-center mt-4">Notre Vision</h2>
           <p className="text-lg text-gray-600 leading-relaxed text-center mt-4">
-          Nous aspirons à devenir l'acteur de référence en France pour la reconversion des athlètes professionnels. Notre ambition est de créer un écosystème bienveillant et efficace, facilitant la transition de chaque sportif vers une nouvelle vie épanouissante. 
+          Nous aspirons à devenir l'acteur de référence en France pour la reconversion des athlètes professionnels. 
+          Notre ambition est de créer un écosystème bienveillant et efficace, facilitant la transition de chaque sportif vers une nouvelle vie épanouissante. 
           </p>
         </div>
       </div>
@@ -70,10 +80,13 @@ const About = () => {
       <Slide direction="left" in={checked} timeout={800}>
         <div className="flex flex-col md:flex-row items-center">
           <div className="text-[#2f4858] text-center md:text-left flex-1">
-            <h6 className="font-bold text-[#6a0dad] text-2xl p-4">Guider les athlètes vers une nouvelle voie épanouissante en visant une transformation globale de leur situation sociale et professionnelle.</h6>
+            <h6 className="font-bold text-[#6a0dad] text-2xl p-4">Guider les athlètes vers une nouvelle
+               voie épanouissante en visant une transformation globale de leur situation sociale et professionnelle.</h6>
             <hr className="border-t border-gray-300" />
             <p className="p-6 mr-7">
-              Cet objectif souligne l'ambition de Trophenix d'aller au-delà d'une simple reconversion professionnelle. Il s'agit de faciliter une véritable transformation de la vie de l'athlète, en l'aidant à trouver un nouveau sens et un épanouissement dans sa vie post-carrière sportive.
+              Cet objectif souligne l'ambition de Trophenix d'aller au-delà d'une simple reconversion professionnelle. 
+              Il s'agit de faciliter une véritable transformation de la vie de l'athlète, en l'aidant à trouver un nouveau sens
+               et un épanouissement dans sa vie post-carrière sportive.
             </p>
           </div>
           
@@ -104,7 +117,7 @@ const About = () => {
   <div className="w-64 h-48 rounded-md shadow-lg">
     {/* Image ou contenu simple pour le test */}
     <img
-      src="/asserts/img/214.png"
+      src="/asserts/img/81.png"
       alt="Image de test"
       className="w-full h-full object-cover rounded"
     />
@@ -141,7 +154,7 @@ const About = () => {
   <div className="w-64 h-48 rounded-md shadow-lg">
     {/* Image ou contenu simple pour le test */}
     <img
-      src="/asserts/img/214.png"
+      src="/asserts/img/102.png"
       alt="Image de test"
       className="w-full h-full object-cover rounded"
     />
@@ -181,7 +194,7 @@ const About = () => {
         {/* Left Image */}
         <div className="md:w-1/2 w-full mb-6 md:mb-0">
           <img
-            src="/asserts/img/116.png"
+            src="/asserts/img/5.png"
             alt="Descriptive Image"
             className="w-full rounded-lg"
           />
@@ -206,14 +219,16 @@ const About = () => {
         <div className="md:w-1/2 w-full md:ml-6 text-center md:text-left">
           <h2 className="text-3xl md:text-4xl  text-[#2f4858] font-bold">Encourager les entreprise à généraliser les emplois pour sportifs</h2>
           <p className="text-lg text-[#2f4858] leading-relaxed mt-4">
-          Nous sensibilisons les entreprises sur les atouts des sportifs de haut niveau, qui possèdent des qualités uniques comme la discipline, la résilience et l’esprit d’équipe. En partenariat avec le secteur privé, nous promouvons la création d’opportunités d'emploi adaptées aux parcours des sportifs, en incitant les entreprises à intégrer ces profils dans leurs équipes et à bénéficier de leur expérience précieuse.
+          Nous sensibilisons les entreprises sur les atouts des sportifs de haut niveau, qui possèdent des qualités uniques comme la discipline,*
+           la résilience et l’esprit d’équipe. En partenariat avec le secteur privé, nous promouvons la création d’opportunités d'emploi adaptées 
+           aux parcours des sportifs, en incitant les entreprises à intégrer ces profils dans leurs équipes et à bénéficier de leur expérience précieuse.
           </p>
         </div>
 
          {/* Left Image */}
          <div className="md:w-1/2 w-full mb-6 md:mb-0">
           <img
-            src="/asserts/img/116.png"
+            src="/asserts/img/80.png"
             alt="Descriptive Image"
             className="w-full rounded-lg"
           />
@@ -227,7 +242,7 @@ const About = () => {
         {/* Left Image */}
         <div className="md:w-1/2 w-full mb-6 md:mb-0">
           <img
-            src="/asserts/img/116.png"
+            src="/asserts/img/81.png"
             alt="Descriptive Image"
             className="w-full rounded-lg"
           />
@@ -241,9 +256,66 @@ const About = () => {
           </p>
         </div>
       </div>
-
-
       </div>
+
+      <div className="flex flex-col md:flex-row items-center justify-between p-8 bg-green-100 p-12" style={{width: '100vw'}}>
+      
+      <div className="relative flex justify-center md:w-1/2 p-4" ref={ref}>
+        <Fade in={inView} timeout={1000}>
+        <div className="relative flex justify-center p-8 ">
+      <div ref={ref} className="relative w-full max-w-[300px] sm:max-w-[400px] lg:max-w-[600px]">
+        
+      
+        {/* Image avec animation de glissement depuis la droite */}
+        <img
+          src="/asserts/img/44.png"
+          alt="Celebrating players"
+          className={`rounded-lg object-cover transition-transform duration-700 ease-in-out ${
+            inView ? 'animate-slide-in-right' : 'opacity-0'
+          }`}
+          style={{ width: "80%", height: "50vh" }}
+        />
+
+        {/* Card avec animation de glissement depuis le bas */}
+        <div
+          className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[80%] sm:w-[60%] lg:w-[50%] max-w-[200px] transition-transform duration-700 ease-in-out ${
+            inView ? 'animate-slide-up' : 'opacity-0'
+          }`}
+        >
+          <div className="bg-white bg-opacity-80 rounded-lg  w-80 p-3 sm:p-4 shadow-lg text-center">
+            <h3 className="text-2xl font-bold text-gray-900">+3000 reconversions/an</h3>
+            <p className="text-gray-600 text-xs sm:text-sm lg:text-base mt-2">
+              80% des sportifs professionnels vivent avec moins de 900€ par mois, et 40% d'entre eux n'ont pas de baccalauréat.
+            </p>
+            <div className="mt-2">
+              <button className="mt-6 px-6 py-2 bg-orange-500 text-white font-semibold rounded-md">
+                France
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        </Fade>
+      </div>
+
+      <div className="md:w-1/2 p-4">
+        <Fade in={inView} timeout={1200}>
+          <div className="text-gray-900">
+            <h2 className="text-3xl md:text-4xl font-bold leading-tight">
+              Devenez des acteurs engagés de la transition <span className="text-orange-500">sportive.</span>
+            </h2>
+            <p className="text-gray-700 mt-4">
+              Rejoignez notre mouvement pour un sport plus durable et inclusif. Chaque action compte, chaque don fait la différence.
+            </p>
+            <button className="mt-6 px-6 py-2 bg-orange-500 text-white font-semibold rounded-md">
+              J'adhère
+            </button>
+          </div>
+        </Fade>
+      </div>
+      
+    </div>
 
 
     </section>
